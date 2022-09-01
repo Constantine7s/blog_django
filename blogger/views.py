078e29986d7c4d8c709dec89bdb9fd6a657ae4bf
Konstantin Shambazov <k.shambazov@gmail.com>
2022-09-01 15:52:34 +0900

    feat: add home, post, about views

--- blogger/views.py
+++ blogger/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Post, Category, Author
 
-# Create your views here.
+def home(request):
+    categories = Category.objects.all()[0:3]
+    featured = Post.objects.filter(featured=True)
+    latest = Post.objects.order_by('-timestamp')[0:3]
+    context = {
+        'object_list': featured,
+        'latest':latest,
+        'categories': categories,
+    }
+    return render(request, 'home.html', context)
+
+def post(request, slug)
+    post = Post.objects.get(slug=slug)
+    context = {'post': post}
+    
+    return render(request, 'post.html', context)
+
+def about (request):
+    return render(request, 'about.html')
